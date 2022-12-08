5edac2515761a44e7ecfbefeeba49c442bd1c3f3
ambrosios <ambrosios@mail.com>
2022-12-08 14:12:07 +0100

    mise a jour

--- algoGenetique/genetique.py
+++
@@ -1,8 +0,0 @@
-
-
-# Tant que critere d'arret
-#     couples = create_couples(individuals, n_individuals)
-#     crossings = crossing(couples)
-#     individuals = mutations(crossings)
-#     individuals = evaluate_and_select(individuals)
-    
