f8a599826aefaafba83279656aa48c0ecdb6577f
ambrosios <ambrosios@mail.com>
2022-12-08 14:10:44 +0100

    test connexion ssh

---
+++ algoGenetique/genetique.py
@@ -0,0 +1,8 @@
+
+
+# Tant que critere d'arret
+#     couples = create_couples(individuals, n_individuals)
+#     crossings = crossing(couples)
+#     individuals = mutations(crossings)
+#     individuals = evaluate_and_select(individuals)
+    
